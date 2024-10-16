28b4a63304d7dcf974d4339e89ee4175b655262b
DaniiGomezz <gomezjesushd@gmail.com>
2024-10-16 15:51:12 -0300

    feat: se configuro el archivo de arranque

---
+++ app.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import vehicleRoutes from './routes/vehicle.route';
+import clientRoutes from './routes/client.route';
+import { MongoVehicleRepository } from './src/repositories/MongoVehicleRepository';
+import { VehicleService } from './services/VehicleService';
+import { ClientRepository } from './repositories/ClientRepository';
+import { ClientService } from './services/ClientService';
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
+
+  private middleware(): void {
+    this.express.use(express.json());
+  }
+
+  private routes(): void {
+
+    this.express.use('/api/vehicles', vehicleRoutes());
+    this.express.use('/api/clients', clientRoutes());
+
+  }
+}
+
+const app = new App().express;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
